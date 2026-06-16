af940f06582186f3bf13a99d5c38f81d4f6fbdc0
HarryYouAreAWizard <136560270+HarryYouAreAWizard@users.noreply.github.com>
2026-06-16 16:45:02 +0200

    documenting

--- monte_carlo.py
+++ monte_carlo.py
@@ -4,7 +4,7 @@
 """
 File: monte_carlo.py
 Author: Noah Nielsen
-Created: 2024-06-01
+Created: 2024-06-14
 Description: This module implements Monte Carlo sampling for converting posterior 
 distributions from Bayesian Inference into point estimates and uncertainty quantification. 
 It provides methods to calculate expected values, variances, credible intervals, and 
@@ -13,13 +13,14 @@ probabilities of differences between groups based on the samples drawn from the 
 should be substituted with a library doing Markov Chain Monte Carlo sampling
 """
 
+"is the monto carlo method just yet another method for doing inference?"
 
 
 import numpy as np
 import scipy
-from scipy.stats import dirichlet, beta
 
 class MonteCarloSampler:
+    """not a MCMC"""
 
     def __init__(self, posterior_dist, num_categories=7, num_samples=100000):
 
@@ -48,9 +49,10 @@ class MonteCarloSampler:
         the Monte Carlo samples."""
         return self.expected_values.var()
     
-
     def probability_dist_of_differences(self, other):
-        """Calculate the probability distribution of the difference in expected values between self and other."""
+        """Calculate the probability distribution of the difference in expected values between self and other.
+        
+        should be replaced with a MCMC"""
 
         self.dif_prop_dist = self.expected_values - other.expected_values 
         return self.dif_prop_dist
@@ -69,6 +71,7 @@ class MonteCarloSampler:
         lower_bound = np.percentile(dif_samples, (0.5 - confidence*0.5) * 100)
         upper_bound = np.percentile(dif_samples, (0.5 + confidence*0.5) * 100)
         return lower_bound, upper_bound
+
     
     def credible_interval_of_differences_gaussian_assumption(self, other, confidence=0.90):
         """Calculate the credible interval for the difference in expected values between self and other,
@@ -87,8 +90,10 @@ class MonteCarloSampler:
         upper_bound = mean_diff + z_score * std_diff
         return lower_bound, upper_bound
 
-    def probability_higher_score_than_other(self, other):
 
-        # Count what fraction of samples are greater than 0
-        prob = np.mean(self.probability_dist_of_differences(other) > 0)
+    def probability_higher_score_than_other(self, other):
+        """Calculate the probabiliity that the mean score from self.samples is higher
+        than the mean score from other.samples."""
+        # count how many samples are greater than 0
+        prob = np.mean(self.dif_prop_dist > 0)
         return prob
